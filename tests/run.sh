b9a179a44c5e07397016568ced0a51c1a96767dd
burgoa <javierburgoac@gmail.com>
2022-01-25 11:37:18 +0100

    delete files that were not deleted on last commit

--- tests/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#SBATCH --cpus-per-task=1
-#SBATCH --output=/g/arendt/Javier/Python/geneannotator/tests/genan.log
-#SBATCH --mem-per-cpu=300MB
-#SBATCH --time=4:00:00
-
-srun python /g/arendt/Javier/Python/geneannotator/geneannotator/orthogroup.py
